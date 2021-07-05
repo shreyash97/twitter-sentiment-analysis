13b90ab25284d9093cb802b3d104546ed3aac2a0
Shreyash <sh639323@dal.ca>
2021-07-04 23:28:59 -0300

    word dictionary added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import final
 import fetch_tweets
 import clean_tweets
 import process
@@ -20,9 +21,18 @@ def analyse(queryTerm, count):
     neg_count = 0
     pos_count = 0
     neu_count = 0
+    totalposwordList = []
+    finalposwordList = []
+    totalnegwords = []
+    finalnegwordList = []
 
     for record in cursor:
-
+        positive_word_list = list(record['positive_words'])
+        negative_word_list = list(record['negative_words'])
+        for i in positive_word_list:
+            totalposwordList.append(i)
+        for i in negative_word_list:
+            totalnegwords.append(i)
         sentiment = record['sentiment']
         sentiment = sentiment.lower()
         if sentiment == 'negative':
@@ -31,8 +41,21 @@ def analyse(queryTerm, count):
             pos_count += 1
         else:
             neu_count += 1
+    for word in totalposwordList:
+        countElem = totalposwordList.count(word)
+        temp = {'value': word, "count": countElem}
+        if temp not in finalposwordList:
+            finalposwordList.append(temp)
+
+    for word in totalnegwords:
+        countNegElem = totalnegwords.count(word)
+        pair = {'value': word, 'count': countNegElem}
+        if pair not in finalnegwordList:
+            finalnegwordList.append(pair)
+
+    # tempList = list(set(finalposwordList))
     res = {'Keyword': queryTerm, 'pos': pos_count,
-           'neg': neg_count, 'neu': neu_count}
+           'neg': neg_count, 'neu': neu_count, 'positiveList': finalposwordList, 'negativeList': finalnegwordList}
     result = jsonify(res)
     result.headers.add('Access-Control-Allow-Origin', '*')
 
